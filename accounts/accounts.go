4aa68c7c3d2513197667b99808fd935e1c431dfa
GH Ryu <gh9503@gmail.com>
2020-06-10 16:43:32 +0900

    Method

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -6,8 +6,21 @@ type Account struct {
 	balance int
 }
 
+//NewAccount is function
 //NewAccount creates Account
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+//Deposit is Method of Account struct
+//Deposit receiver는 struct의 첫글자를 따서 소문자로 해야함(ex a Account)
+//Deposit x amount on your account
+func (a Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+//Balance of your account
+func (a Account) Balance() int {
+	return a.balance
 }
