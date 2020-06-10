54bae01f18b9a77ac7d452218fcd00dbc69319c9
GH Ryu <gh9503@gmail.com>
2020-06-10 16:55:31 +0900

    Method pointer & error&nil return

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,14 @@
 package accounts
 
+import "errors"
+
 //Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't withdraw")
 
 //NewAccount is function
 //NewAccount creates Account
@@ -16,11 +20,20 @@ func NewAccount(owner string) *Account {
 //Deposit is Method of Account struct
 //Deposit receiver는 struct의 첫글자를 따서 소문자로 해야함(ex a Account)
 //Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 //Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+//Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
