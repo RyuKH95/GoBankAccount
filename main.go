99e879205fbd2ac396e3313a856807655997aff1
GH Ryu <gh9503@gmail.com>
2020-06-10 17:13:55 +0900

    Account print String change

--- main.go
+++ main.go
@@ -9,10 +9,6 @@ import (
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance())
+	account.ChangeOwner("nicolas")
+	fmt.Println(account)
 }
